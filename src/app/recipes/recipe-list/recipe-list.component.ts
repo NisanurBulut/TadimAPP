408ede2e4c1517227744e96ca853f7e5bd1fde66
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-04 20:27:57 +0300

    tarif listeleme çalışması başlamıştır.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe []=[
+  // tslint:disable-next-line: no-unused-expression
+  // tslint:disable-next-line: align
+  new Recipe('Karnıyarık','Karnıyarık yemeği','https://www.misssgibi.com/media/scaled/2019/10/16/karniyarik-tarifi-w1600.jpg')
+];
   constructor() { }
 
   ngOnInit() {
