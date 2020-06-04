4c1ac2bf36f88509671515b11198cb21f5dc5c11
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-04 10:50:59 +0300

    component kaydı appmodule yapıldı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
