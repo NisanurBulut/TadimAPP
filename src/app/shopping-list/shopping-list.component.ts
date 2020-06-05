3f04544ddb6dcc222ae9871d6468487ed4bce07e
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-05 11:58:10 +0300

    içerik listesinin görünümü için html tasarımı yapıldı.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients:[];
   constructor() { }
 
   ngOnInit() {
