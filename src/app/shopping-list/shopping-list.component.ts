b190e379034e542364dc2d4876a3151294f39e7a
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-05 12:15:20 +0300

    içerik listesinin gmrüntülenmesi için html tasarımı yapıldı.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-ingredients:[];
+ingredients:Ingredient[]=[
+  new Ingredient('patlıcan',6),
+  new Ingredient('soğan',1)
+];
   constructor() { }
 
   ngOnInit() {
