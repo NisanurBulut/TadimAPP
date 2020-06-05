b11ef478ef361e66fcb817f6328dc3248c2b8e9e
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-05 12:05:07 +0300

    Ingredient model yazıldı

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    public name: string;
+    public amount: number;
+    // tslint:disable-next-line: variable-name
+    constructor(_name: string, _amount: number) {
+this.amount = _amount;
+this.name = _name;
+    }
+}
