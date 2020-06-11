3bf07be050cfe24dd1e43ab9de116e293c24ae42
NisanurBulut <nisanurrunasin@gmail.com>
2020-06-11 16:28:32 +0300

    yemekler, servis kullanılarak listelendi.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component,EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,13 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-recipes:Recipe []=[
-  new Recipe('Karnıyarık','Karnıyarık yemeği','https://www.misssgibi.com/media/scaled/2019/10/16/karniyarik-tarifi-w1600.jpg')
-];
+recipes:Recipe [];
+
 @Output() recipeWasSelected=new EventEmitter<Recipe>();
-  constructor() { }
+
+  constructor(private rService:RecipeService ) { }
 
   ngOnInit() {
+    this.recipes=this.rService.getRecipes();
   }
   onRecipeSelected(receipeSelected:Recipe){
     //dışarıye verir
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService{
+import { Recipe } from './recipe.model';
 
+export class RecipeService{
+  private  recipes:Recipe []=[
+        new Recipe(
+          'Karnıyarık',
+        'Karnıyarık yemeği',
+        'https://www.misssgibi.com/media/scaled/2019/10/16/karniyarik-tarifi-w1600.jpg')
+      ];
+      getRecipes() {
+          return this.recipes.slice();
+      }
 }
