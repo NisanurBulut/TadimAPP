12e2081779efaccbbcb14f0c9af5ee140b8c61af
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-02 14:32:55 +0300

    yemek tarifi eklendi.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
   public recipeSelected=new EventEmitter<Recipe>();
@@ -8,7 +9,15 @@ export class RecipeService{
         new Recipe(
           'Karnıyarık',
         'Karnıyarık yemeği',
-        'https://www.misssgibi.com/media/scaled/2019/10/16/karniyarik-tarifi-w1600.jpg')
+        'https://www.misssgibi.com/media/scaled/2019/10/16/karniyarik-tarifi-w1600.jpg',
+        [new Ingredient('Kabak',1),
+         new Ingredient('Domates',1)]),
+         new Recipe(
+          'Sarma Dolma',
+        'Salma Dolma',
+        'https://i.ytimg.com/vi/w7B3xSnspYg/maxresdefault.jpg',
+        [new Ingredient('Pirinç',1),
+         new Ingredient('Maydonoz',1)])
       ];
       getRecipes() {
           return this.recipes.slice();
