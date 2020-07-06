9589a32c58bd7cae7019af9e44c489c8f00d2221
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-06 14:52:15 +0300

    edit ekran için router link verildi.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,8 @@ export class RecipeDetailComponent implements OnInit {
 recipe: Recipe;
 id: number;
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router ) { }
 
   ngOnInit() {
     // router'dan gelen id değerini alalım aşağıdaki yaklaşım doğru değil
@@ -20,14 +21,15 @@ id: number;
     this.route.params.subscribe(
       (params: Params) => {
       this.id = +params['id'];
-      this.recipe=this.recipeService.getRecipe(this.id);
+      this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
   }
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-  onEditRecipe(){
-
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 }
