5cf6cd0d8c35dbbea52ba7c9d779880ddfd9660d
NisanurBulut <nisanurrunasin@gmail.com>
2020-07-06 14:26:51 +0300

    editmode ve edit ekran için id gönderimi yazıldı

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+id: number;
+editMode:false;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // id değerini almak için
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        // bir id değeri varsa düzenleme modundadır
+        this.editMode = params['id'] != null;
+      }
+    );
   }
 
 }
