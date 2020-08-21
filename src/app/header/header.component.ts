edffb89108fa334fb65fd8c0638f501aefeb0e19
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-21 14:47:58 +0300

    veri kaydı yapıldı

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component,EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 
 })
 export class HeaderComponent {
-
-
+    constructor(private dss: DataStorageService) { }
+    onSaveData() {
+        this.dss.storeRecipes();
+     }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private rs: RecipeService) { }
+    storeRecipes() {
+        const recipes = this.rs.getRecipes();
+        console.log('servise geldi');
+        return this.http.put('https://tadimapp-ca4fc.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            }, error => {
+                console.log(error);
+            });
+    }
 }
