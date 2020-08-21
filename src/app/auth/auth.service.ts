af77222d3f73f9800bfd6e95dce22e289b716dcd
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-21 21:50:06 +0300

    login yazıldı

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -26,18 +26,7 @@ export class AuthService {
             password: ppassword,
             returnSecureToken: true
         }
-        ).pipe(
-            catchError(errRes => {
-                let errMessage = 'Bilinmeyen bir hata ile karşılaşıldı.';
-                if (!errRes.error || !errRes.error.error) {
-                    return throwError(errMessage);
-                }
-                switch (errRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errMessage = 'Bu mail adresi zaten kayıtlıdır.';
-                }
-                return throwError(errMessage);
-            }));
+        ).pipe(catchError(this.handleError));
     }
     login(pemail: string, ppassword: string) {
         return this.httpClient.post<AuthResponseData>(
@@ -47,16 +36,24 @@ export class AuthService {
             returnSecureToken: true
         }
         ).pipe(
-            catchError(errRes => {
-                let errMessage = 'Bilinmeyen bir hata ile karşılaşıldı.';
-                if (!errRes.error || !errRes.error.error) {
-                    return throwError(errMessage);
-                }
-                switch (errRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errMessage = 'Bu mail adresi zaten kayıtlıdır.';
-                }
-                return throwError(errMessage);
-            }));
+            catchError(this.handleError));
+    }
+    private handleError(errorRes: HttpErrorResponse) {
+        let errMessage = 'Bilinmeyen bir hata ile karşılaşıldı.';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errMessage = 'Bu mail adresi zaten kayıtlıdır.';
+                break;
+            case 'INVALID_EMAIL':
+                errMessage = 'Bu mail adresi tanımlı değildir.';
+                break;
+            case 'INVALID_PASSWORD':
+                errMessage = 'Bu parola tanımlı değildir.';
+                break;
+        }
+        return throwError(errMessage);
     }
 }
