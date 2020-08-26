3870c9de4d9878d8e8d7c88d9f782a5eb2d3d87b
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-26 11:45:15 +0300

    update ve delete ingredients işlem için action yazımı yapıldı

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
     constructor(public payload: Ingredient) {
@@ -14,5 +16,17 @@ export class AddIngredients {
     readonly type = ADD_INGREDIENTS;
     constructor(public payload: Ingredient[]) { }
 }
+export class UpdateIngredient implements Action {
+    readonly type = UPDATE_INGREDIENT;
+    constructor(public payload: { index: number, ingredient: Ingredient }) {
+
+    }
+}
+export class DeleteIngredient implements Action {
+    readonly type = DELETE_INGREDIENT;
+    constructor(public payload: number) {
+
+    }
+}
 // multiple action olduğu için payload erişimi olabilmesi adına bu tanımlama yapıldı
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export type ShoppingListActions = AddIngredient | AddIngredient | UpdateIngredient | DeleteIngredient;
