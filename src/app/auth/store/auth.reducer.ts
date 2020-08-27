ea44ff1bd6697441296a973438b603e5c5a9ffd1
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-27 21:50:19 +0300

    auth actions yazıldı

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,9 +1,31 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
     user: User;
 }
 const initialState: State = { user: null };
-export function authReducer(state = initialState, action) {
-    return state;
- }
+export function authReducer(
+    state = initialState,
+    action: AuthActions.AuthActions) {
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            const userEntity = new User(
+                action.payload.email,
+                action.payload.userId,
+                action.payload.token,
+                action.payload.expirationDate
+            );
+            return {
+                ...state,
+                user: userEntity
+            };
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
