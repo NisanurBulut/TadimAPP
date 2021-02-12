0279e776511c784dc3151912a68f4918c12d6285
NisanurBulut <nbulut@sarplojistik.com>
2021-02-12 20:52:54 +0300

    implemented logging service

--- src/app/Logging.service.ts
+++ src/app/Logging.service.ts
@@ -1,1 +1,10 @@
-export class ShoppingService { }
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn:'root'})
+export class LoggingService  {
+    lastLog:string;
+    printLog(message:string){
+        this.lastLog=message;
+        console.log(message);
+    }
+}
