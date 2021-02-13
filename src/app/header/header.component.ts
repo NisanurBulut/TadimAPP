2a682ef45bf2632c153254487b96050722f099fd
NisanurBulut <nbulut@sarplojistik.com>
2021-02-13 17:40:37 +0300

    header restyled

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,8 +9,8 @@ import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
-
+    templateUrl: './header.component.html',
+    styleUrls:['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
     private userSub: Subscription;
