a41341d0033f35a12efbc93573bafe88bac2046b
NisanurBulut <nbulut@sarplojistik.com>
2021-02-14 11:13:52 +0300

    implemented recipe actions

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -3,15 +3,34 @@ import { Recipe } from '../recipe.model';
 
 export const SET_RECIPES = '[Recipes] Set Recipes';
 export const FETCH_RECIPES = '[Recipes] Fetch Recipes';
+export const ADD_RECIPE = '[Recipe] Add Recipe';
+export const UPDATE_RECIPE = '[Recipe] Update Recipe';
+export const DELETE_RECIPE = '[Recipe] Delete Recipe';
 
+export class DeleteRecipe implements Action {
+    readonly type = DELETE_RECIPE;
+    constructor(public payload: number) {
+    }
+}
+export class AddRecipe implements Action {
+    readonly type = ADD_RECIPE;
+    constructor(public payload: Recipe) { }
+}
+export class UpdateRecipe implements Action {
+    readonly type = UPDATE_RECIPE;
+    constructor(public payload: { index: number, newRecipe: Recipe }) { }
+}
 export class FetchRecipes implements Action {
     readonly type = FETCH_RECIPES;
 }
 export class SetRecipes implements Action {
     readonly type = SET_RECIPES;
     constructor(public payload: Recipe[]) {
-
     }
 }
 
-export type RecipesActions = SetRecipes | FetchRecipes;
+export type RecipesActions = SetRecipes |
+    FetchRecipes |
+    AddRecipe |
+    UpdateRecipe |
+    DeleteRecipe;
