c9119c658417e0c94667ab28286ab95a3a0ed886
NisanurBulut <nbulut@sarplojistik.com>
2021-02-14 11:22:06 +0300

    implemented recipe reducer

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,5 +1,4 @@
-import { InitialState } from "@ngrx/store/src/models";
-import { Recipe } from "../recipe.model";
+import { Recipe } from '../recipe.model';
 import * as fromRecipesActions from './recipe.actions';
 
 export interface State {
@@ -7,13 +6,38 @@ export interface State {
 }
 const initialState: State = {
     recipes: []
-}
+};
+
 export function recipeReducer(state: State = initialState, action: fromRecipesActions.RecipesActions) {
     switch (action.type) {
         case fromRecipesActions.SET_RECIPES:
             return {
                 ...state,
                 recipes: [...action.payload]
+            };
+        case fromRecipesActions.ADD_RECIPE:
+            return {
+                ...state,
+                recipes: [...state.recipes, action.payload]
+            };
+        case fromRecipesActions.UPDATE_RECIPE:
+            const updatedRecipe = {
+                ...state.recipes[action.payload.index],
+                ...action.payload.newRecipe
+            };
+            const updatedRecipes = [...state.recipes];
+            updatedRecipes[action.payload.index] = updatedRecipe;
+
+            return {
+                ...state,
+                recipes: updatedRecipes
+            };
+        case fromRecipesActions.DELETE_RECIPE:
+            return {
+                ...state,
+                recipes: state.recipes.filter((recipe, index) => {
+                    return index !== action.payload;
+                })
             };
         default:
             return state;
