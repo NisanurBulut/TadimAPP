b292a48bef49f0e60f18431933adcc2f6001727d
NisanurBulut <nbulut@sarplojistik.com>
2021-02-14 10:42:13 +0300

    recipe-resolver fixed with ngrx

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -6,23 +6,21 @@ import {
 } from '@angular/router';
 
 import { Recipe } from './recipe.model';
-import { DataStorageService } from '../shared/data-storage.service';
-import { RecipeService } from './recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as fromRecipeActions from './store/recipe.actions';
+import { Store } from '@ngrx/store';
+import { Actions, ofType } from '@ngrx/effects';
+import { take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipesService: RecipeService
-  ) {}
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
+  ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipesService.getRecipes();
-
-    if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-    } else {
-      return recipes;
-    }
+    this.store.dispatch(new fromRecipeActions.FetchRecipes());
+    return this.actions$.pipe(ofType(fromRecipeActions.SET_RECIPES), take(1));
   }
 }
