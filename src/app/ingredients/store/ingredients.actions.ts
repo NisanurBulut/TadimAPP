45cad669152c3989a200354e8bf0b94c6c9ebdf8
NisanurBulut <nbulut@sarplojistik.com>
2021-02-17 19:18:28 +0300

    implemented ingredients actions

--- src/app/ingredients/store/ingredients.actions.ts
+++ src/app/ingredients/store/ingredients.actions.ts
@@ -2,14 +2,47 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 
+export const LOAD_INGREDIENTS = '[Ingredient] Load Ingredients';
+export const LOAD_INGREDIENTS_SUCCESS = '[Ingredient] Load Ingredients Success';
+export const LOAD_INGREDIENTS_FAIL = '[Ingredient] Load Ingredients Fail';
+
 export const ADD_INGREDIENT = '[Ingredient] Add Ingredient';
 export const ADD_INGREDIENT_SUCCESS = '[Ingredient] Add Ingredient Success';
 export const ADD_INGREDIENT_FAIL = '[Ingredient] Add Ingredient Fail';
 
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const DELETE_INGREDIENT = '[INGREDIENT] Delete Ingredient';
+export const DELETE_INGREDIENT_SUCCESS = '[INGREDIENT] Delete Ingredient Success';
+export const DELETE_INGREDIENT_FAIL = '[INGREDIENT] Delete Ingredient Fail';
+
+export const UPDATE_INGREDIENT = '[Ingredient] Update Ingredient';
+export const UPDATE_INGREDIENT_SUCCESS = '[Ingredient] Update Ingredient Success';
+export const UPDATE_INGREDIENT_FAIL = '[Ingredient] Update Ingredient Fail';
+
+export class UpdateRecipe implements Action {
+    readonly type = UPDATE_INGREDIENT;
+    constructor(public payload: Ingredient) { }
+}
+export class UpdateRecipeSuccess implements Action {
+    readonly type = UPDATE_INGREDIENT_SUCCESS;
+    constructor(public payload: Ingredient) {
+    }
+}
+export class UpdateRecipeFail implements Action {
+    readonly type = UPDATE_INGREDIENT_FAIL;
+    constructor(public payload: Error) { }
+}
+
+export class LoadIngredients implements Action {
+    readonly type = LOAD_INGREDIENTS;
+}
+export class LoadIngredientsSuccess implements Action {
+    readonly type = LOAD_INGREDIENTS_SUCCESS;
+    constructor(public payload: Array<Ingredient>) { }
+}
+export class LoadIngredientFail implements Action {
+    readonly type = LOAD_INGREDIENTS_FAIL;
+    constructor(public payload: Error) { }
+}
 
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
@@ -27,25 +60,27 @@ export class AddIngredientFail implements Action {
     constructor(public payload: Error) { }
 }
 
+export class DeleteIngredientSuccess implements Action {
+    readonly type = DELETE_INGREDIENT_SUCCESS;
+    constructor(public payload: number) {
+    }
+}
+export class DeleteIngredientFail implements Action {
+    readonly type = DELETE_INGREDIENT_FAIL;
+    constructor(public payload: Error) { }
+}
+export class DeleteIngredient implements Action {
+    readonly type = DELETE_INGREDIENT;
+    constructor(public payload: number) {
+    }
+}
 export class UpdateIngredient implements Action {
     readonly type = UPDATE_INGREDIENT;
     constructor(public payload: Ingredient ) {
     }
 }
-export class DeleteIngredient implements Action {
-    readonly type = DELETE_INGREDIENT;
 
-}
-export class StartEdit implements Action {
-    readonly type = START_EDIT;
-    constructor(public payload: number) {
-    }
-}
-export class StopEdit implements Action {
-    readonly type = STOP_EDIT;
-    constructor() {
-    }
-}
+
 // multiple action olduğu için payload erişimi olabilmesi adına bu tanımlama yapıldı
 export type IngredientActions =
     AddIngredient |
