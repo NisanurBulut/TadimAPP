3671356475a8e33b49fc72c06bf5ba55803b954b
NisanurBulut <nbulut@sarplojistik.com>
2021-02-17 18:51:25 +0300

    implemented ingredient http calls

--- src/app/ingredients/ingredients.service.ts
+++ src/app/ingredients/ingredients.service.ts
@@ -1,41 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Ingredient } from '../shared/ingredient.model';
 
+@Injectable()
 export class IngredientsService {
-  // eventemitter subjet'e dönüştü
-  ingredientsChanged = new Subject<Ingredient[]>();
-  // subject sınıfı generic tipte
-  startedEditing = new Subject<number>();
-  private ingredients: Ingredient[] = [
-    new Ingredient('Domates', 5),
-    new Ingredient('Kabak', 1),
-  ];
-  getIngredients() {
-    return this.ingredients.slice();
+  constructor(private http: HttpClient) {
   }
-  getIngredient(index: number) {
-    return this.ingredients[index];
+
+  getIngredients() {
+    return this.http.get<Array<Ingredient>>(environment.apiIngredientUrl).pipe(delay(500));
+  }
+  getIngredient(id: number) {
+    return this.http.get(`${environment.apiIngredientUrl}/${id}`)
+      .pipe(delay(500));
   }
   addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-    // emit next'e dönüştü
-    this.ingredientsChanged.next(this.ingredients.slice());
+    return this.http.post(environment.apiIngredientUrl, ingredient)
+      .pipe(delay(500));
   }
-  addIngredients(ingredients: Ingredient[]) {
-    // for (let ingredient of ingredients) {
-    //   this.addIngredient(ingredient);
-    // }
-    this.ingredients.push(...ingredients);
-    // emit next'e dönüştü
-    this.ingredientsChanged.next(this.ingredients.slice());
+
+  updateIngredient(newIngredient: Ingredient) {
+    return this.http.put(`${environment.apiIngredientUrl}/${newIngredient.id}`, newIngredient)
+    .pipe(delay(500));
   }
-  updateIngredient(index: number, newIngredient: Ingredient) {
-    // mevcut malzemeyi alalım
-    this.ingredients[index] = newIngredient;
-    this.ingredientsChanged.next(this.ingredients.slice());
-  }
-  deleteIngredient(index: number) {
-    this.ingredients.splice(index, 1);
-    this.ingredientsChanged.next(this.ingredients.slice());
+  deleteIngredient(id: number) {
+    return this.http.delete(`${environment.apiIngredientUrl}/${id}`)
+      .pipe(delay(500));
   }
 }
