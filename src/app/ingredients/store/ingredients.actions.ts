583d1d45b53c6cf4ded6da570fab6aba4b9e3430
NisanurBulut <nbulut@sarplojistik.com>
2021-02-17 19:02:13 +0300

    implemented ingredient add action

--- src/app/ingredients/store/ingredients.actions.ts
+++ src/app/ingredients/store/ingredients.actions.ts
@@ -2,23 +2,31 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const ADD_INGREDIENT = '[Ingredient] Add Ingredient';
+export const ADD_INGREDIENT_SUCCESS = '[Ingredient] Add Ingredient Success';
+export const ADD_INGREDIENT_FAIL = '[Ingredient] Add Ingredient Fail';
+
 export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 export const START_EDIT = 'START_EDIT';
 export const STOP_EDIT = 'STOP_EDIT';
+
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
     constructor(public payload: Ingredient) {
-
     }
 }
-// önce action tanımı yapılır
-export class AddIngredients implements Action {
-    readonly type = ADD_INGREDIENTS;
-    constructor(public payload: Ingredient[]) { }
+
+export class AddIngredientSuccess implements Action {
+    readonly type = ADD_INGREDIENT_SUCCESS;
+    constructor(public payload: Ingredient) {
+    }
 }
+export class AddIngredientFail implements Action {
+    readonly type = ADD_INGREDIENT_FAIL;
+    constructor(public payload: Error) { }
+}
+
 export class UpdateIngredient implements Action {
     readonly type = UPDATE_INGREDIENT;
     constructor(public payload: Ingredient ) {
@@ -41,7 +49,8 @@ export class StopEdit implements Action {
 // multiple action olduğu için payload erişimi olabilmesi adına bu tanımlama yapıldı
 export type IngredientActions =
     AddIngredient |
-    AddIngredients |
+    AddIngredientSuccess |
+    AddIngredientFail |
     UpdateIngredient |
     DeleteIngredient |
     StartEdit |
--- src/app/ingredients/store/ingredients.reducer.ts
+++ src/app/ingredients/store/ingredients.reducer.ts
@@ -4,19 +4,16 @@ import * as fromIngredientActions from './ingredients.actions';
 
 export interface State {
     ingredients: Ingredient[];
-    editedIngredient: Ingredient;
-    editedIngredientIndex: number;
+    loading: boolean;
+    error: Error;
 }
 export interface AppState {
     ingredientList: State;
 }
 const initialState: State = {
-    ingredients: [
-        new Ingredient('Domates', 5),
-        new Ingredient('Kabak', 1),
-    ],
-    editedIngredient: null,
-    editedIngredientIndex: -1
+    ingredients: [],
+    loading: false,
+    error: null
 };
 // buradaki actiona aslında bir interface
 export function IngredientsReducer(
@@ -26,50 +23,13 @@ export function IngredientsReducer(
         case fromIngredientActions.ADD_INGREDIENT:
             return {
                 ...state,
-                ingredients: [...state.ingredients, action.payload]
+                ingredients: [...state.ingredients, action.payload],
+                loading: true
             };
-        case fromIngredientActions.ADD_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: [...state.ingredients, ...action.payload]
-            };
-        case fromIngredientActions.UPDATE_INGREDIENT:
-            const ingredient = state.ingredients[state.editedIngredientIndex];
-            const updatedIngredient = {
-                ...ingredient,
-                ...action.payload
-            };
-            const updatedIngredients = [...state.ingredients];
-            updatedIngredients[state.editedIngredientIndex] = updatedIngredient;
-            // ... spread operator
-            return {
-                ...state,
-                ingredients: updatedIngredients,
-                editedIngredientIndex: -1,
-                editedIngredient: null
-            };
-        case fromIngredientActions.DELETE_INGREDIENT:
-            // ... spread operator
-            return {
-                ...state,
-                ingredients: state.ingredients.filter((ig, igIndex) => {
-                    return igIndex !== state.editedIngredientIndex;
-                })
-            };
-        case fromIngredientActions.START_EDIT:
-            // ... spread operator
-            return {
-                ...state,
-                editedIngredientIndex: action.payload,
-                editedIngredient: { ...state.ingredients[action.payload] }
-            };
-        case fromIngredientActions.STOP_EDIT:
-            // ... spread operator
-            return {
-                ...state,
-                editedIngredientIndex: -1,
-                editedIngredient: null
-            };
+        case fromIngredientActions.ADD_INGREDIENT_SUCCESS:
+                return { ...state, ingredients: [...state.ingredients, action.payload], loading: false };
+        case fromIngredientActions.ADD_INGREDIENT_FAIL:
+                return { ...state, error: action.payload, loading: false };
         default:
             return state;
     }
